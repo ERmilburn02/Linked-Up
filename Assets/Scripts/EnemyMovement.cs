6472d5063a5d5486263404de3347f1b280f150d1
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2021-06-12 18:57:33 +0100

    push current code to github

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody rb;
+    Vector2 inputV;
+
+    public bool reverseHorizontalInput = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!reverseHorizontalInput)
+        {
+            inputV.x = Input.GetAxisRaw("Horizontal");
+        }
+        else
+        {
+            inputV.x = -(Input.GetAxisRaw("Horizontal"));
+        }
+        inputV.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector3(inputV.x, 0, inputV.y) * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody rb;
+    Vector2 inputV;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        inputV.x = Input.GetAxisRaw("Horizontal");
+        inputV.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector3(inputV.x, 0, inputV.y) * moveSpeed * Time.fixedDeltaTime);
+    }
+}
